f18301311d5bc83769ededd97a09eb9fcf11da80
Chaoran Yang <chaorany@me.com>
2016-09-18 01:12:02 +0000

    Make example works.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,13 @@
-const [ webpack, path ] = [
-    require('webpack'),
-    require('path')
-];
-
-const [ SRC_DIR, PUBLIC_DIR ] = [
-    path.resolve(__dirname, 'src'),
-    path.resolve(__dirname, 'public')
-];
-
 module.exports = {
-    entry: path.resolve(SRC_DIR, 'index.jsx'),
+    entry: './src/app.jsx',
     output: {
-        path: PUBLIC_DIR,
-        filename: 'bundle.js'
+        path: './bin',
+        publicPath: '/bin/',
+        filename: 'app.bundle.js'
     },
     module: {
         loaders: [
-            {
-                test: /\.jsx?/,
-                include: SRC_DIR,
-                loader: 'babel'
-            },
+            { test: /\.jsx?$/, include: __dirname + '/src', loader: 'babel' },
         ],
     },
 };
