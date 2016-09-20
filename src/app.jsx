b2c2c76c2dc8565c88879d521355cd47503ef715
Chaoran Yang <chaorany@me.com>
2016-09-20 05:06:10 +0000

    Clickable navs.

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { render } from 'react-dom';
+import _ from 'lodash';
 import Nav from './nav.jsx';
 
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize.min.js';
 
+class Link {
+    constructor(text) {
+        this.text = text;
+        this.id = _.camelCase(text);
+    }
+}
+
+const links = [ new Link('About Me') ];
+
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { active: null };
+        this.setActive = (link) => {
+            this.setState({ active: link });
+        };
+    }
+
     render() {
-        return <Nav />
+        return <Nav
+            links={ links } active={ this.state.active }
+            onClick={ this.setActive }
+        />
     }
 }
 
--- src/nav.jsx
+++ src/nav.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import _ from 'lodash';
 
 class Nav extends React.Component {
+    constructor() {
+        super();
+        this.clickHome = () => this.props.onClick(null);
+    }
+
     render() {
+        const links = this.props.links.map((link) => {
+            const isActive = link == this.props.active ? 'active' : '';
+            const click = () => { this.props.onClick(link); };
+            return (
+                <li key={ link.id } className={ isActive }>
+                    <a href={ '#' + link.id } onClick={ click }>
+                        { link.text }
+                    </a>
+                </li>
+            );
+        });
         return (
             <nav>
                 <div className="container nav-wrapper">
-                    <a href="#" className="brand-logo">Chaoran Yang</a>
+                    <a
+                        href="#"
+                        className="brand-logo"
+                        onClick={ this.clickHome }>
+                        Chaoran Yang
+                    </a>
                     <ul id="nav-mobile" className="right hide-on-small-only">
-                        <li><a href="#about">About</a></li>
+                        { links }
                     </ul>
                 </div>
             </nav>
